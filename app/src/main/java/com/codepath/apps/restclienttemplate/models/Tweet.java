7e98d31f85f8ce8d88f714e2854563ff59bc0a35
molinarodri <lauramolinapdx@gmail.com>
2019-11-08 20:25:25 -0500

    Implemented compose tweet feature

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -3,9 +3,11 @@ package com.codepath.apps.restclienttemplate.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
+@Parcel
 
 public class Tweet {
     public String body;
@@ -13,6 +15,9 @@ public class Tweet {
     public long id;
     public User user;
 
+    // empty constructor needed by the Parceler library
+    public Tweet() {
+    }
 
     public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
         Tweet tweet = new Tweet();
